87147f471f5abca3f737c860713dbcfa2564427e
esmaltecordecoco <00001096929314sp@al.educacao.sp.gov.br>
2024-05-23 16:22:51 -0300

    Update and rename README.md to sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600, 600);
+  background("rgb(227,142,247)")
+}
+
+function draw() {
+   
+  stroke("rgb(83,83,219)");
+  fill("rgb(151,101,228)");
+  
+  if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
+ 
